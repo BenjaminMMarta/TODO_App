bd44a3743b40882ec44a756243c68b93e8a84b3e
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-09 17:09:10 -0700

    Added some styling to styles.css and altered some comments in script.js

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const todoItem = document.querySelector('#newTodoListItem');
 //Local Storage Variables.
 let todoList = JSON.parse(localStorage.getItem('listItem'));
 
-
-//Loop through to do list array.
+//Loop through todo list array.
 for (let i =0; i<todoList.length; i++) {
    console.log(todoList[i]);
    const createNewTodo = document.createElement('li');
@@ -24,8 +23,7 @@ for (let i =0; i<todoList.length; i++) {
    todoItem.appendChild(createNewTodo);
 };
 
-
-//"Submit New Todo" button event listener which creates and adds new todo in an unordered list.
+//"Submit" button event listener which creates and adds new todo in an unordered list.
 todoForm.addEventListener('submit', function(event) {
     event.preventDefault();
     const createNewTodo = document.createElement('li');
@@ -40,7 +38,7 @@ todoForm.addEventListener('submit', function(event) {
     createNewTodo.appendChild(deleteTodo);
     todoItem.appendChild(createNewTodo);
     newTodo.value = '';
-//local storage put to do list items in local storage.
+//local storage put todo list items in local storage.
     todoList.push(newTodoText.innerHTML);
     localStorage.setItem('listItem', JSON.stringify(todoList));
 
